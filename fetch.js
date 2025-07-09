2e1b841b1b05a1a87545ca4dd31fd68b92c82e76
MNSH23 <cyruspradhan666@gmail.com>
2025-07-09 12:13:21 +0530

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,6 @@
+<html lang=""en">
+<body>
+<script>
+const target= document.getElementById("target");
+var data = fetch ( "https://jsonplaceholder.typicode.com/posts?_limit=12/1");
+val2.forEach(element=> {
